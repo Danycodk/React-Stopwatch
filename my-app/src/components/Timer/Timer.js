f45dadf95819c67f57271d037520830d598f3afd
danycodk <danielkonate123@gmail.com>
2023-10-27 14:54:09 -0400

    minutes,secondes and hundredth secondes can be start pause and clear

--- my-app/src/components/Timer/Timer.js
+++ my-app/src/components/Timer/Timer.js
@@ -1,33 +1,61 @@
 import './timer.css';
 import React, { useState } from 'react';
 function Timer() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);//Hundredth Seconde
+  const [countSeconde, setcountSeconde] = useState(0) //second
+  const [countMinute, setcountMinute] = useState(0) //Minutes
+  const [intervalId, setIntervalId] = useState(0);
+  //const [booTrueFalse, setBooTrueFalse] = useState(true);
+
+  //let functionTriggered = false;
+
 
     const handleClick = () => {
-      setInterval(() => {
-       setCount((count) => count + 1);
-      }, 1000);
+      //functionTriggered = true;
+
+      let newIntervalId =setInterval(() => {
+        setCount((count) => count + 1);
+        /*if(count === 100){
+          setCount(0)
+          setcountSeconde((countSeconde) => countSeconde + 1);
+        }*/
+       }, 10);
+       setIntervalId(newIntervalId);
+     }
+     //////////////////////////////////////////////////
+     if(count === 100){
+      setCount(0)
+      setcountSeconde((countSeconde) => countSeconde + 1);
+    }
+    /////////////////////////////////////////////////
+    if(countSeconde === 60){
+      setcountSeconde(0)
+      setcountMinute((countMinute) => countMinute + 1);
+    }
+    /////////////////////////////////////////////////
+     const stopCountHandler = () => {
+      clearInterval(intervalId);
      };
 
-   // return () => {
-    //  clearInterval(intervalId); 
-  //  };
-  //}, []); 
-  function habdlerStop(){
-    clearInterval(handleClick)
+  function habdleReset(){
+    setCount(0)
+    setcountSeconde(0)
+    setcountMinute(0)
   }
  // onClick={handleClick}
     return (
       <>
-      
+      <br/>
+      <h1> after</h1>
       <section>
        
-        <div class="hours">{count}</div>
-        <div class="minutes"></div>
-        <div class="secondes"></div>
+        <div class="hours">{countMinute} :</div>
+        <div class="minutes">{countSeconde}:</div>
+        <div class="secondes">{count}</div>
       </section>
       <button  onClick={handleClick}>Start event</button><br/>
-      <button  onClick={habdlerStop}>Clear event</button>
+      <button  onClick={stopCountHandler }>Pause event</button><br/>
+      <button  onClick={habdleReset}>Clear event</button>
       </>
     );
   }
